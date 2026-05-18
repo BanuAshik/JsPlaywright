a6dced0cfbfb9d801b79063dc1e268c34a68eaac
Banu Ashik <abrosebanu8@gmail.com>
2026-05-18 14:42:27 +0400

    Update HomeassignemnteWeek1.js

--- homeassignment/HomeassignemnteWeek1.js
+++ homeassignment/HomeassignemnteWeek1.js
@@ -4,25 +4,25 @@
 4. Return the corresponding grade.
 5. Call the function and print the result.*/
 
-function Studentscore(marks){
+function Studentscore() {
 
-    switch(true)
-    {
-        case (marks>=70 &&marks<=80):
+    switch (true) {
+        case (marks >= 70 && marks <= 80):
             console.log("grade C")
             break;
-            case (marks>=80 &&marks<=90):
-                console.log("grade B")
-                break;
-                case (marks>=90 &&marks<=100):
-                console.log("grade A")
-                break;
-                default:
-                    console.log("grade D fail")
-                        break;
-                
+        case (marks >= 80 && marks <= 90):
+            console.log("grade B")
+            break;
+        case (marks >= 90 && marks <= 100):
+            console.log("grade A")
+            break;
+        default:
+            console.log("grade D fail")
+            break;
+
     }
 }
-                        let marks=75;
-                        console.log(Studentscore(marks));
+let marks = 75;
+Studentscore(marks);
+
 
